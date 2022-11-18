d197e8541480930b4d4c95a8a742f3d12a5cc2a4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 20:58:54 +0000

    Adding tarfile member sanitization to extractall()

--- device.py
+++ device.py
@@ -91,7 +91,26 @@ def put_calibration(devid: uuid.UUID):
                 filename, request.headers.get('Signature', ''), devid)
         try:
             with tarfile.open(filename) as tf:
-                tf.extractall(path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, path)
             os.unlink(filename)
         except:
             raise error.APISyntaxError('Bad tarball')
